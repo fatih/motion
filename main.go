60c829ac08d2764c2d2cfb490e41f88d887229e1
Fatih Arslan <ftharsln@gmail.com>
2016-03-02 22:05:52 +0200

    astcontext: some refactoring, not the last

--- main.go
+++ main.go
@@ -12,17 +12,6 @@ import (
 	"github.com/fatih/motion/vim"
 )
 
-var (
-	flagFile   = flag.String("file", "", "Filename to be parsed")
-	flagOffset = flag.String("offset", "", "Byte offset of the cursor position")
-	flagMode   = flag.String("mode", "", "Running mode. One of {enclosing, next, prev}")
-	flagShift  = flag.Int("shift", 0, "Shift value for the modes {next, prev}")
-	flagFormat = flag.String("format", "gnu",
-		"Output format. One of {gnu, json, vim}")
-	flagParseComments = flag.Bool("parse-comments", false,
-		"Parse comments and add them to AST")
-)
-
 func main() {
 	if err := realMain(); err != nil {
 		fmt.Fprint(os.Stderr, err.Error())
@@ -31,6 +20,17 @@ func main() {
 }
 
 func realMain() error {
+	var (
+		flagFile   = flag.String("file", "", "Filename to be parsed")
+		flagOffset = flag.String("offset", "", "Byte offset of the cursor position")
+		flagMode   = flag.String("mode", "", "Running mode. One of {enclosing, next, prev}")
+		flagShift  = flag.Int("shift", 0, "Shift value for the modes {next, prev}")
+		flagFormat = flag.String("format", "gnu",
+			"Output format. One of {gnu, json, vim}")
+		flagParseComments = flag.Bool("parse-comments", false,
+			"Parse comments and add them to AST")
+	)
+
 	flag.Parse()
 
 	if *flagOffset == "" {
@@ -57,7 +57,9 @@ func realMain() error {
 		return err
 	}
 
+	var funcs astcontext.Funcs
 	var fn *astcontext.Func
+
 	switch *flagMode {
 	case "enclosing":
 		fn, err = parser.Funcs().EnclosingFunc(offset)
@@ -65,28 +67,48 @@ func realMain() error {
 		fn, err = parser.Funcs().Declarations().NextFuncShift(offset, *flagShift)
 	case "prev":
 		fn, err = parser.Funcs().Declarations().PrevFuncShift(offset, *flagShift)
+	case "funcs":
+		// TODO(arslan): change the scope from file to package
+		funcs = parser.Funcs()
 	default:
 		return fmt.Errorf("wrong mode %q passed", *flagMode)
 	}
 
-	var res interface{} = fn
-
 	// do no return, instead pass it to the editor so it can parse it
 	if err != nil {
+		return printErr(*flagFormat, err)
+	}
+
+	if fn != nil {
+		funcs = append(funcs, fn)
+	}
+
+	return printResult(*flagFormat, funcs)
+}
+
+func printErr(mode string, err error) error {
+	var res interface{}
+	switch mode {
+	case "json", "vim":
 		res = struct {
 			Err string `json:"err" vim:"err"`
 		}{
 			Err: err.Error(),
 		}
+	case "gnu":
+		res = err
 	}
 
-	switch *flagFormat {
-	case "json", "gnu", "vim":
-	default:
-		return fmt.Errorf("wrong -format value: %q.\n", *flagFormat)
+	return printResult(mode, res)
+}
+
+func printResult(mode string, res interface{}) error {
+	// if it contains only one function, return it as a single function
+	if funcs, ok := res.(astcontext.Funcs); ok && len(funcs) == 1 {
+		res = funcs[0]
 	}
 
-	switch *flagFormat {
+	switch mode {
 	case "json":
 		b, err := json.MarshalIndent(&res, "", "\t")
 		if err != nil {
@@ -103,9 +125,17 @@ func realMain() error {
 		switch x := res.(type) {
 		case *astcontext.Func:
 			fmt.Println(x)
+		case astcontext.Funcs:
+			for _, fn := range x {
+				fmt.Println(fn)
+			}
+		case error:
+			fmt.Println(x.Error())
 		default:
-			fmt.Println(err.Error())
+			fmt.Println(res)
 		}
+	default:
+		return fmt.Errorf("wrong -format value: %q.\n", mode)
 	}
 
 	return nil
